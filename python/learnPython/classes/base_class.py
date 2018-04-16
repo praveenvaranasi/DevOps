0eda735e526a1fee5e763eaf8da3fe44c1001b0d
praveen <praveenb.varanasi@outlook.com>
2018-04-16 21:54:03 +0530

    Added class files

--- python/learnPython/classes/base_class.py
+++ python/learnPython/classes/base_class.py
@@ -12,27 +12,32 @@ class base:
         try:
             string_sample = "I'm non local"
             print("The word is {0}".format(string_sample))
-            print(string_sample[0:12])
+            print(string_sample[0:13])
         except ValueError:
             print('Provide corresponding data type value to the string_sample')
 
-def design():
-    for i in range(0,40):
-        print('*',end='')
-    # print('\n')
+    def design(self, sym):
+        for i in range(0, 40):
+            print(sym, end='')
+
+
+class derived(base):
+    base.string_slice("")
+
+
 
 def main_mod():
 
-
-    design()
+    character = str(input("Enter a symbol: "))
+    base.design(' ' ,character)
     print("\nInvoking the class using Instance Object")
-    design()
+    base.design('',character)
     Obj = base()
     Obj.string_slice()
     print(Obj.__doc__, Obj.string_slice.__doc__)
-    design()
+    base.design('', character)
     print('\nInvoking the Class using Class Object')
-    design()
+    base.design('', character)
     print('\n'+str(base.sample))
     base.string_slice('hello')
 
